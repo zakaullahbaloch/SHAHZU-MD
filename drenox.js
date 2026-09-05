966a2a06901af5b9dae256a5514b91a137dc2980
zakaullahbaloch <1.68470039e+08+zakaullahbaloch@users.noreply.github.com>
2026-09-05 09:41:03 +0000

    Allow sudo users to use owner commands

--- drenox.js
+++ drenox.js
@@ -11657,7 +11657,7 @@ break
 
 case 'jadu':
 case 'vv': {
-  if (!isBot) return reply('❌ owner only')
+  if (!isCreator) return reply('❌ owner only')
   if (!m.quoted) {
     return reply(`*ʀᴇᴘʟʏ ᴛᴏ ᴀɴ ɪᴍᴀɢᴇ, ᴠɪᴅᴇᴏ, ᴏʀ ᴀᴜᴅɪᴏ ᴡɪᴛʜ ᴛʜᴇ ᴄᴀᴘᴛɪᴏɴ ${prefix + command}*`)
   }
